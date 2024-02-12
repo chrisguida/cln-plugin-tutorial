d85377c315adf8954f964e155729eddcf6b6796e
Chris Guida <chrisguida@gmail.com>
2024-02-12 16:13:59 -0600

    fix venv, fix helloworld.py location

---
+++ venv/lib/python3.10/site-packages/pyln/client/__init__.py
@@ -0,0 +1,19 @@
+from .lightning import LightningRpc, RpcError, Millisatoshi
+from .plugin import Plugin, monkey_patch, RpcException
+from .gossmap import Gossmap, GossmapNode, GossmapChannel, GossmapNodeId
+
+__version__ = "23.02"
+
+__all__ = [
+    "LightningRpc",
+    "Plugin",
+    "RpcError",
+    "RpcException",
+    "Millisatoshi",
+    "__version__",
+    "monkey_patch",
+    "Gossmap",
+    "GossmapNode",
+    "GossmapChannel",
+    "GossmapNodeId",
+]
